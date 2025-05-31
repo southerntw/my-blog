47af083fa39d74a4a84064322e1e7e70f84034d9
southerntw <fauzanmasnur.a@gmail.com>
2025-05-31 12:21:02 +0700

    post: ace combat 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.fauzanmansur.id/",
+	site: "https://fauzanmansur.id/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
